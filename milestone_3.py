6c056b800c3be3a7f320833f2d6f5d548fdfcdcd
neketaaa <nikita16060415@gmail.com>
2023-07-18 20:25:09 +0300

    Checking if input letter is in the chosen word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,9 @@
+import random
+
+word_list = ['Apple', 'Banana', 'Mango', 'Orange', 'Peach']
+
+word = random.choice(word_list)
+
 while True:
     guess = input('Enter a single letter: ')
 
@@ -5,3 +11,8 @@ while True:
         break
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
+
+if guess in word:
+    print(f'Good guess! {guess} is in the word.')
+else:
+    print(f'Sorry, {guess} is not in the word. Try again.')
