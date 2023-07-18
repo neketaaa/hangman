9ae1ce3d82b942de9142c4d2aba5b713a61a28de
neketaaa <nikita16060415@gmail.com>
2023-07-18 20:21:08 +0300

    Creating infinite loop to get correct input

---
+++ milestone_3.py
@@ -0,0 +1,7 @@
+while True:
+    guess = input('Enter a single letter: ')
+
+    if len(guess) == 1 and guess.isalpha():
+        break
+    else:
+        print('Invalid letter. Please, enter a single alphabetical character.')
